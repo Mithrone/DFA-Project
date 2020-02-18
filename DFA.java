8bc66ff13b52a0aa49d003c1aff353d2975c6cc8
Zander van Tonder <zandermarko.vantonder@gmail.com>
2020-02-18 16:21:21 +0200

    FFFFFFFFFFFFFFFFff

--- DFA.java
+++ DFA.java
@@ -1,16 +1,18 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.lang.Thread.State;
+import java.text.StringCharacterIterator;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class DFA
 {
-    int letterSize;
-    int conSize;
-    int linkSize;
+    int statesCount;
+    int edgesCount;
+    int transitionsCount;
 
     final String[] posName = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","BB","CC","DD","EE","FF","GG","HH","II","JJ","KK","LL","MM","NN","OO","PP","QQ","RR","SS","TT","UU","VV","WW","XX","YY","ZZ"};
 
-    int[][] pos;
+    
     DFA(String file)
     {
         readFile(file);
@@ -33,29 +35,18 @@ public class DFA
                 if(firstLine)
                 {
                     String[] val = line.split(" ");
-                    letterSize = Integer.parseInt(val[0]);
-                    conSize = Integer.parseInt(val[1]);
-                    linkSize = Integer.parseInt(val[2]);
+                    statesCount = Integer.parseInt(val[0]);
+                    edgesCount = Integer.parseInt(val[1]);
+                    transitionsCount = Integer.parseInt(val[2]);
 
                     firstLine = false;
                 }
                 else
                 {
                     String[] val = line.split(" ");
-                    pos = new int[letterSize][2];
                     
-                    for(int i = 0; i< posName.length; i++)
-                    {
-                        if(val[0].equals(posName[i]))
-                        {
-                            pos[count][0] = i;
-                        }
-
-                        if(val[2].equals(posName[i]))
-                        {
-                            pos[count][1] = i;
-                        }
-                    }
+                    
+                    
                     count++;
                      
                 }
@@ -67,6 +58,49 @@ public class DFA
         {
             System.out.println("An error occurred.");
             e.printStackTrace();
+        }
+    }
+
+
+    class States
+    {
+        int edgeCount = 1;
+        String[] edges = new String[edgeCount];
+
+        int conCount = 1;
+        String[] connected = new String[conCount];
+        
+        States()
+        {
+
+        }
+
+        public void addEdge(String e)
+        {
+            String[] temp = edges;
+
+            edges = new String[++edgeCount];
+
+            for(int i = 0; i< edgeCount - 1; i++)
+            {
+                edges[i] = temp[i];
+            }
+
+            edges[edgeCount] = e;
+        }
+
+        public void addConnection(String c)
+        {
+            String[] temp = edges;
+
+            connected = new String[++conCount];
+
+            for(int i = 0; i< conCount - 1; i++)
+            {
+                edges[i] = temp[i];
+            }
+
+            connected[conCount] = c;
         }
     }
 }
